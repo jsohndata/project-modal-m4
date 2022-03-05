281362c27d135c5f181bab35e0df36d440243291
Jiho Sohn <jihos@me.com>
2022-03-04 19:40:01 -0500

    update-javascript
    making improvement to the JS file
    
    adding function to pass multiple variables to external JS file

--- script-modal-m4.js
+++ script-modal-m4.js
@@ -1,8 +1,21 @@
-// Create const
-const popModal = new bootstrap.Modal(document.getElementById('popModal'), {
-    keyboard: true  
-});
 
+// Const passed var
+const passVarName  = document.getElementById('passVar');
+const passVarValue = passVarName.getAttribute("data-search");
+
+// Split string using comma
+const passVarArray = passVarValue.split(",");
+
+// Assign the value to variables
+const elementID     = passVarArray[0];
+const keyboardState = Boolean(passVarArray[1]);
+
+alert (keyboardState);
+
+// Const for Modal
+const popModal = new bootstrap.Modal(document.getElementById('popModal'), {
+    keyboard: keyboardState  
+});
 
 // Load the modal
 function popModalLoad () {
@@ -12,25 +25,23 @@ function popModalLoad () {
 
 // Set the Blur On
 function blurOn() {
-    document.querySelector('#section').classList.add('blur-on');
+    document.querySelector(elementID).classList.add('blur-on');
     return true;
 }
 
 // Set the Blur Off
 function blurOff() {
-    document.querySelector('#section').classList.add('blur-off');
+    document.querySelector(elementID).classList.add('blur-off');
     return true;
 }
 
-
-
 // Onload ... 
 window.addEventListener('load', (event) => {
-    popModalLoad() ? console.log('modal => 1') : console.log('modal => 0')
-    blurOn()       ? console.log('blur-on => 1') : console.log('blur-on => 0')
+    popModalLoad()  ? console.log('modal => 1') : console.log('modal => 0')
+    blurOn()        ? console.log('blur-on => 1') : console.log('blur-on => 0')
 });        
 
 // When the modal close ... 
 window.addEventListener('hidden.bs.modal', (event) => {
-    blurOff() ? console.log('blur-off => 1') : console.log('blur-off => 0')
+    blurOff()       ? console.log('blur-off => 1') : console.log('blur-off => 0')
 });
